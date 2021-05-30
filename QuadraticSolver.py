6290456e60c0a82f8e12005a8e05a3a6900b066b
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-05-30 19:13:45 +0100

    Now adds complex root solutions and allows input for floats.

--- QuadraticSolver.py
+++ QuadraticSolver.py
@@ -1,19 +1,33 @@
-def if_real_solution(root):
-    if root < 0:
-        print("This will have a complex solution.")
-    elif root == 0:
-        print("This solution will have a repeated root.")
+import math
+
+
+# Generates real and complex solutions for a quadratic polynomial
+def solution(var_a, var_b, to_root):
+    if to_root > 0:
+        sol_1 = (-1 * var_b + math.sqrt(to_root)) / (2 * var_a)
+        sol_2 = (-1 * var_b - math.sqrt(to_root)) / (2 * var_a)
+        print(f"The solutions are:\n{sol_1}\n{sol_2}")
+    elif to_root == 0:
+        sol_rep = (-1 * var_b + math.sqrt(to_root)) / (2 * var_a)
+        print(f"The solution to this quadratic equation is:\n{sol_rep}")
     else:
-        print("This solution will have two distinct roots.")
-    print(root)
+        real_coefficient = (-1 * var_b) / (2 * var_a)
+        complex_coefficient_1 = math.sqrt(-1 * to_root) / (2 * var_a)
+        complex_coefficient_2 = -(1 * math.sqrt(-1 * to_root)) / (2 * var_a)
+        sol_com_1 = complex(real_coefficient, complex_coefficient_1)
+        sol_com_2 = complex(real_coefficient, complex_coefficient_2)
+        print(f"The solutions are:\n{sol_com_1}\n{sol_com_2}")
 
-a = input("Welcome to this polynomial solver. Please place in the coefficients of your quadratic where appropriate.\n"
-          "This is in the format of ax^2 + bx + c = 0. Enter the coefficients as required below.\n a:")
-b = input("b:")
-c = input("c:")
 
-d = 4 * int(a) * int(c)
-e = int(b) ** 2
-square_root = d-e
+# Coefficients of the polynomial
+a = float(
+    input("Welcome to this polynomial solver. Please place in the coefficients of your quadratic where appropriate.\n"
+          "This is in the format of ax^2 + bx + c = 0. Enter the coefficients as required below.\n a:"))
+b = float(input("b:"))
+c = float(input("c:"))
 
-if_real_solution(square_root)
+# Test to determine whether real or complex roots are generated
+root_sol = b ** 2 - 4 * a * c
+
+# Calls the function
+solution(a, b, root_sol)
