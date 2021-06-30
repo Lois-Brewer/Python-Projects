fb3e9af7d644dbe818438abf5ce1540f0f2159f9
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-06-30 20:41:11 +0100

    Cleared most lines and set up a function determining if a string is a number.

--- Mini Projects/NumberGuessingGame.py
+++ Mini Projects/NumberGuessingGame.py
@@ -1,29 +1,15 @@
 import random
 
 
-def other_guess():
-    return input("Please try another number:")
-
-
-def hint(guess):
-    if guess < random_number:
-        print("That number is too low!")
-        other_guess()
-    elif guess > random_number:
-        print("That's too high!")
-        other_guess()
+def is_numeric(string):
+    if string.isnumeric():
+        return int(string)
     else:
-        return "Congratulations! That is correct!"
+        print("This is not a number!")
 
 
 random_number = random.randint(1, 100)
 print("Hello and welcome to the Number Guessing Game!\nI will choose a random number between 1 and 100"
       " and you have to guess it! Good Luck!")
 
-initial_guess = input("Initial Guess:")
-
-if initial_guess.isnumeric():
-    initial_guess = int(initial_guess)
-    hint(initial_guess)
-else:
-    print("You didn't input a number.")
+first_guess = input("Please input your first number:")
