53eb7a04b305cd1bb9142c242373c162bbd38dcf
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-07-02 13:12:23 +0100

    Fixed issue where starting a number with 0 causes the program to break.

--- Mini Projects/Calculator.py
+++ Mini Projects/Calculator.py
@@ -3,15 +3,22 @@ import re
 
 def filter(equation_string):
     # Filters input string to an equation
-    equation_string = re.findall("[0-9^*%/\+\-]+", equation_string)
+    equation_string = re.findall("[0-9^*%/\+\-.]+", equation_string)
     filtered_eq = ''.join(equation_string)
     return filtered_eq
 
 
 def calculation(filtered_equation):
     # Calculates the answer
-    answer = eval(filter(filtered_equation))
-    return answer
+    for i in range(len(filtered_equation)):
+        try:
+            answer = eval(filter(filtered_equation))
+            return answer
+        except filtered_equation[i] == '0' and SyntaxError:
+            filtered_equation = filtered_equation.replace(str(i), '')
+            return eval(filter(filtered_equation))
+    print(filtered_equation)
+
 
 
 # Input string
