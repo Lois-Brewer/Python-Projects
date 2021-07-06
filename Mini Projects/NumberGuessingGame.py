f39c886c6eed01e6a82374c1b4c959330ebfbdd0
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-07-06 18:56:15 +0100

    Update on NumberGuessingGame.py
    Generates random number in a user specified range, compares this random number with user's initial guess.

--- Mini Projects/NumberGuessingGame.py
+++ Mini Projects/NumberGuessingGame.py
@@ -2,14 +2,48 @@ import random
 
 
 def is_numeric(string):
+    # Checks if input is numeric
     if string.isnumeric():
         return int(string)
     else:
-        print("This is not a number!")
+        return f"{string} is not a number!"
 
 
-random_number = random.randint(1, 100)
-print("Hello and welcome to the Number Guessing Game!\nI will choose a random number between 1 and 100"
-      " and you have to guess it! Good Luck!")
+def make_random_number(specified_range):
+    # Generates random number with specified range
+    random_number = random.randint(1, is_numeric(specified_range))
+    return random_number
 
-first_guess = input("Please input your first number:")
+
+def guess(computer_guess, user_guess, maximum):
+    # Compares the randomly generated number and user's guess
+    if user_guess < maximum + 1:
+        if computer_guess == user_guess:
+            print(f"Congratulations! {user_guess} is the correct number!")
+        elif computer_guess > user_guess:
+            print(f"{user_guess} is too low!")
+        elif computer_guess < user_guess:
+            print(f"{user_guess} is too high!")
+    else:
+        print(f"{user_guess} is higher than the maximum range you have previously specified.")
+
+
+print("Hello and welcome to the Number Guessing Game!")
+
+upper = input("Insert the number you would like to guess up to:")
+upper_limit = is_numeric(upper)
+
+try:
+    if upper_limit == int(upper_limit):
+        print(f"Guess a number in the range of 1 and {upper_limit}.")
+except ValueError:
+    # Terminates program if upper_limit isn't an int
+    print("An error has occurred.")
+    quit()
+
+# Assigns random number
+my_guess = make_random_number(upper)
+
+# Assigns user's guess and compares with random number
+first_guess = is_numeric(input("Please input your first number:"))
+guess(my_guess, first_guess, upper_limit)
