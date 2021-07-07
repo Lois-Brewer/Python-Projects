129818c7f08ecb2bc5c04e69cff68de08ea963ab
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-07-07 15:59:02 +0100

    NumberGuessingGame.py functioning
    Set up a counter and now has limited number of guesses. Needs minor tweaks but otherwise complete.

--- Mini Projects/NumberGuessingGame.py
+++ Mini Projects/NumberGuessingGame.py
@@ -15,15 +15,37 @@ def make_random_number(specified_range):
     return random_number
 
 
-def guess(computer_guess, user_guess, maximum):
+def counter(count_level, comp_guess, maximum):
+    while count_level >= 0:
+        if count_level == 0:
+            print(f"You have run out of guesses. The answer was {comp_guess}.")
+            quit()
+        elif count_level == 1:
+            print(f"You have {count_level} guess left. Please try another guess.")
+            new_guess = input("What is your next guess?")
+            final_guess = is_numeric(new_guess)
+            guess(comp_guess, final_guess, maximum, count_level)
+        else:
+            print(f"You have {count_level} guesses left. Please try another guess.")
+            new_guess = input("What is your next guess?")
+            new_guess1 = is_numeric(new_guess)
+            guess(comp_guess, new_guess1, maximum, count_level)
+
+
+def guess(computer_guess, user_guess, maximum, counts):
     # Compares the randomly generated number and user's guess
     if user_guess < maximum + 1:
         if computer_guess == user_guess:
             print(f"Congratulations! {user_guess} is the correct number!")
+            quit()
         elif computer_guess > user_guess:
+            counts -= 1
             print(f"{user_guess} is too low!")
+            counter(counts, computer_guess, maximum)
         elif computer_guess < user_guess:
+            counts -= 1
             print(f"{user_guess} is too high!")
+            counter(counts, computer_guess, maximum)
     else:
         print(f"{user_guess} is higher than the maximum range you have previously specified.")
 
@@ -45,5 +67,12 @@ except ValueError:
 my_guess = make_random_number(upper)
 
 # Assigns user's guess and compares with random number
-first_guess = is_numeric(input("Please input your first number:"))
-guess(my_guess, first_guess, upper_limit)
+first_guess = input("Please input your first number:")
+first_guess1 = is_numeric(first_guess)
+
+try:
+    if first_guess1 == int(first_guess):
+        count = 3
+        guess(my_guess, first_guess1, upper_limit, count)
+except ValueError:
+    print(is_numeric(first_guess))
