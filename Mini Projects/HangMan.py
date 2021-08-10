c134e2573dc512e21732d6b6470fd01bb3037f4e
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-08-10 15:30:10 +0100

    Now works for multiple guesses, displays game over or correct word. Functioning game.

--- Mini Projects/HangMan.py
+++ Mini Projects/HangMan.py
@@ -4,16 +4,24 @@ r = RandomWords()
 hangman_word = r.get_random_word(hasDictionaryDef="true", minLength=5, maxLength=10)
 hangman_cha_list = list(enumerate(hangman_word.lower()))
 output_str = "-" * len(hangman_word)
-
-
-def test(character_guess, output, enumerated_guess):
-    for i, cha in enumerated_guess:
-        if character_guess == cha:
-            output = output[:i] + cha + output[i + 1:]
-    return output
+running = True
+count = 5
 
 
 print("Welcome to Hangman! Please note that the word length will be between 5-10 characters long.")
-cha_guess = input("Input guess:")
 
-print(test(cha_guess, output_str, hangman_cha_list))
+while running:
+    cha_guess = input("Input guess:")
+    for i, cha in hangman_cha_list:
+        if cha_guess == cha:
+            output_str = output_str[:i] + cha + output_str[i + 1:]
+    print(output_str)
+    if cha_guess not in [x[1] for x in hangman_cha_list]:
+        count -= 1
+        print(f"You have {count} guesses left!")
+        if count == 0:
+            print(f"The game is over. The correct word was {hangman_word}.")
+            running = False
+    elif "-" not in output_str:
+        print(f"Congratulations! You have guessed the correct word, {hangman_word}.")
+        running = False
