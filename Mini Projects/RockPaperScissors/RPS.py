9c7f917742a860c80c68d7e1c11f4c7c9d9d2793
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-09-09 15:03:41 +0100

    Completed game
    Completed game of rock, paper, scissors. Minor fixes and refactored code.

--- Mini Projects/RockPaperScissors/RPS.py
+++ Mini Projects/RockPaperScissors/RPS.py
@@ -1,49 +1,37 @@
 import random as r
 
-# NOTE: CODE IS FUNCTIONING BUT STILL BEING WORKED ON.
-# Thank you for your patience.
+
+# Sets up required variables
 running = True
 user_wins = 0
 comp_wins = 0
 answers = ["R", "P", "S"]
+win_combos = ["PR", "RS", "SP"]
 
+
+# Welcome message
 print("Welcome to Rock-Paper-Scissors. Please input one of the following:"
       "\n'R' - rock\n'P' - paper\n'S' - scissors\nto get started.")
 
 
 while running:
+    # Running game of rock, paper, scissors
+    if user_wins == 3 or comp_wins == 3:
+        print(f"Game is over. The score was {user_wins}-{comp_wins}. Thanks for playing.")
+        break
     user_guess = input("Guess:").upper()
     if user_guess.upper() not in answers:
         print("You didn't enter a valid letter.")
-        running = False
-    user_guess = user_guess.upper()
+        break
     comp_guess = answers[r.randint(0, 2)]
-    print(comp_guess)
     guess_join = user_guess + comp_guess
     if guess_join[0] == guess_join[1]:
-        print(f"You both guessed {user_guess}.\nThe current score is {user_wins}-{comp_wins}.")
-    elif user_wins == 3 or comp_wins == 3:
-        print(f"Game is over. The score was {user_wins}-{comp_wins}. Thanks for playing.")
-        break
+        print(f"You both guessed {user_guess}!\nThe current score is {user_wins}-{comp_wins}.")
     else:
-        if guess_join == "RP" or guess_join == "PR":
-            if guess_join[0] == "R":
-                comp_wins += 1
-                print(f"You lose! Score is {user_wins}-{comp_wins}.")
-            else:
-                user_wins += 1
-                print(f"You win! Score is {user_wins}-{comp_wins}.")
-        elif guess_join == "RS" or guess_join == "SR":
-            if guess_join[0] == "S":
-                comp_wins += 1
-                print(f"You lose! Score is {user_wins}-{comp_wins}.")
-            else:
-                user_wins += 1
-                print(f"You win! Score is {user_wins}-{comp_wins}.")
+        # Checks to see if computer or user has won the round.
+        if any(guess_join == elem in win_combos for elem in win_combos):
+            user_wins += 1
+            print(f"You win! Score is {user_wins}-{comp_wins}.")
         else:
-            if guess_join[0] == "P":
-                comp_wins += 1
-                print(f"You lose! Score is {user_wins}-{comp_wins}.")
-            else:
-                user_wins += 1
-                print(f"You win! Score is {user_wins}-{comp_wins}.")
+            comp_wins += 1
+            print(f"You lose! Score is {user_wins}-{comp_wins}.")
