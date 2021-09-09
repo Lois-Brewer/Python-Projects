97c194c723b0e4a182fbe52a99554fe32ab58e06
Snowystar122 <43116592+Snowystar122@users.noreply.github.com>
2021-09-09 15:12:58 +0100

    Refactored code and added comments.

--- Mini Projects/AlternatingCaps/AlternatingCaps.py
+++ Mini Projects/AlternatingCaps/AlternatingCaps.py
@@ -1,18 +1,19 @@
 def changing_caps(s):
+    # Creates string with alternating caps, letters only
     count = 0
     output_str = ""
     for i in s:
         if i.isalpha() and (count % 2 == 0 or count == 0):
             count += 1
-            i = i.upper()
-            output_str += i
+            output_str += i.upper()
         elif i.isalpha() and count % 2 != 0:
             count += 1
-            i = i.lower()
-            output_str += i
+            output_str += i.lower()
         else:
             output_str += i
     return output_str
 
 
-print(changing_caps(input("Input your string here:\n")))
+# Asks for input and returns output here
+input_str = input("Input your string here:\n")
+print(changing_caps(input_str))
